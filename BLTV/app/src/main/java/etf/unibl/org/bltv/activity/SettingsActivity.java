2accd3d197f43574b7a99770047ed36eef196073
mr.dzoviii <joco-95@hotmail.com>
2018-06-15 00:06:04 +0200

    Radi se restart aplikacije na promjenu jezika.

--- BLTV/app/src/main/java/etf/unibl/org/bltv/activity/SettingsActivity.java
+++ BLTV/app/src/main/java/etf/unibl/org/bltv/activity/SettingsActivity.java
@@ -75,8 +75,14 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
         }
     }
     private void restartActivity() {
-        Intent intent = getIntent();
+        /*Intent intent = new Intent(this,SplashActivity.class);
         finish();
-        startActivity(intent);
+        startActivity(intent);*/
+        Intent i = getBaseContext().getPackageManager().
+                getLaunchIntentForPackage(getBaseContext().getPackageName());
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        finish();
+        startActivity(i);
     }
 }
